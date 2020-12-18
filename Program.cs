72dfc0867a61ec1852c4d2f2223d76b11c49f10b
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-12-18 17:41:03 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AulaPOO_Interface.classes;
+
+namespace AulaPOO_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carrinho carrinho = new Carrinho();
+
+            Produto p1 = new Produto();
+            p1.Codigo = 001;
+            p1.Nome = "Nike Air Force 1";
+            p1.Preco = 600;
+
+            Produto p2 = new Produto();
+            p2.Codigo = 002;
+            p2.Nome = "Nike Jordan 1";
+            p2.Preco = 799;
+
+            Produto p3 = new Produto();
+            p3.Codigo = 003;
+            p3.Nome = "Adidas Yeezy 350";
+            p3.Preco = 1200;
+
+            carrinho.Cadastrar(p1);
+            carrinho.Cadastrar(p2);
+            carrinho.Cadastrar(p3);
+            carrinho.Listar();
+            Console.ForegroundColor = ConsoleColor.Green;
+            carrinho.Alterar(p1.Codigo,p3);
+            System.Console.WriteLine("Lista alterada:");
+            carrinho.Listar();
+            Console.ResetColor();
+        }
+    }
+}
---
+++ classes/ICarrinho.cs
@@ -0,0 +1,13 @@
+namespace AulaPOO_Interface.classes
+{
+    public interface ICarrinho
+    {
+        void Listar();
+
+        void Cadastrar(Produto produto);
+
+        void Alterar(int _codigo, Produto produto);
+         
+        void Deletar(Produto produto);
+    }
+}
